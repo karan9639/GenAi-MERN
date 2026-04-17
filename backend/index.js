e88faef857d4f2c5636ec0b45678226ce1cb58e1
Karan Singh <kvnisad18114982@gmail.com>
2026-04-17 17:44:48 +0530

    i use ES Modules with use "type": "module" so i can easily use modern import export features

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const app = require("./src/app");
+import { app } from './src/app.js';
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
 
-exports = app;
+export default app;
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,5 @@
-const express = require('express');
+import express from 'express'
 
 const app = express();
 
-
-module.exports = app;
+export {app};
